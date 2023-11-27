d00262e11dd1cb7be0da73b19608b8417aba2cc2
yosekit <yamshchikov_k@mail.ru>
2023-11-27 16:20:49 +0300

    Fix labs.

--- lab2/task2.sh
+++ lab2/task2.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
+n=$(cat ./.myevents.conf)
+n=${n:-7}
+
 if [ $# -eq 0 ]; then
-  n=$(cat ./.myevents.conf)
-  n=${n:-7}
   tail -n $n ./.myevents
 else
   if [ $1 = "-d" ]; then
     start_date=$2
-    awk -v date="$start_date" '$1 >= date' ./.myevents
+    awk -v date="$start_date" '$1 >= date' ./.myevents | head -n $n
   else
     echo "$(date +'%Y-%m-%d %H:%M:%S') $*" >> ./.myevents
   fi
